fe3d8b8b8c0fd0e37e0e415bda31171295be8b4c
Yang Tun-Kai <ks97121@gmail.com>
2017-07-16 01:27:59 +0800

    remove if

--- index.php
+++ index.php
@@ -175,9 +175,7 @@ class Index{
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        if(!empty($this->input['entry'][0]['messaging'][0]['message'])){
-            $result = curl_exec($ch);
-        }
+        $result = curl_exec($ch);
     }
 }
 
