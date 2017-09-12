a5b24e8d2501a268cf50249ea20d3d44dcdd17d4
Yang Tun-Kai <ks97121@gmail.com>
2017-09-12 23:50:58 +0800

    comment some reply text

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ class Index{
             $this->message_to_reply = '嗨，我是 Taiwan Baseball App Facebook 聊天小精靈。你可以在這邊問我關於戰績、球員、賽事相關的情報唷。輸入 help 以取得資訊。';
             
         }else{
-            $this->message_to_reply = '不好意思，暫時無法回答你的問題。可以再多給我一點提示嗎？或是輸入 help 查詢。或者等等小編來回答你。';
+            //$this->message_to_reply = '不好意思，暫時無法回答你的問題。可以再多給我一點提示嗎？或是輸入 help 查詢。或者等等小編來回答你。';
         }
         
         $this->send_message($this->message_to_reply);
